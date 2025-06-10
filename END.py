10ff092fc7c2d12bf0c63866c6fd1b6b23b1923c
ShiLongFu <fuslon@126.com>
2025-06-10 22:27:31 +0800

    Update END.py

--- END.py
+++ END.py
@@ -69,7 +69,7 @@ if st.button("Predict"):
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
 
-    shap.force_plot(explainer.expected_value, shap_values, pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
+    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
     
     plt.savefig("shap_force_plot.tif", bbox_inches='tight', dpi=1200)
   
